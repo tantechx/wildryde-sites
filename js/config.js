caa4c3ee70673f625eefbf5dee0afa6d97b13cd6
Roarke <willman0031@gmail.com>
2020-06-15 10:27:50 +0700

    First commit

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_U2dj4WkN3', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2l432d2ock4fibqevg1vrsk1vh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://jsqr0ilhbe.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
